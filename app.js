ef7732dc581d2a6f7d51993b562276f16b38a55c
Andy Wang <andywang@Andys-MacBook-Pro.local>
2023-01-31 14:53:57 -0800

    removed app.get

--- app.js
+++ app.js
@@ -66,8 +66,4 @@ app.get("/api/searchByUser", async (req, res) => {
 	}
 });
 
-app.get("*", async (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
 app.listen(port);
